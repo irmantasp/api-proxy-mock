b7004c5cc18416dd8cc20e3c232088295237394e
Irmantas Puodžiukas <irmantas.puodziukas@gmail.com>
2021-12-15 16:10:41 +0200

    #00 Change validation for form post header

--- src/Proxy/Adapter/GuzzleAdapter.php
+++ src/Proxy/Adapter/GuzzleAdapter.php
@@ -2,6 +2,7 @@
 
 namespace App\Proxy\Adapter;
 
+use Laminas\Diactoros\ServerRequestFactory;
 use \Proxy\Adapter\Guzzle\GuzzleAdapter as BaseGuzzleAdapter;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -12,16 +13,16 @@ class GuzzleAdapter extends BaseGuzzleAdapter
     final public function send(RequestInterface $request): ResponseInterface
     {
         $method = $request->getMethod();
-        if ($method === 'POST' && !$this->isFormPost($request)) {
+        if ($method === 'POST' && !$this->isFormPost()) {
             return parent::send($request);
         }
 
         return $this->client->request($method, $this->getUri($request), $this->getData($request));
     }
 
-    private function isFormPost(RequestInterface $request): bool
+    private function isFormPost(): bool
     {
-        $contentType = $request->getHeaderLine('Content-Type');
+        $contentType = $this->getHeaderLine('Content-Type');
 
         return (strpos($contentType, 'form-data') !== false);
     }
@@ -32,6 +33,11 @@ class GuzzleAdapter extends BaseGuzzleAdapter
             'headers' => $request->getHeaders(),
             'form_params' => $this->getFormPostParams(),
         ];
+    }
+
+    private function getHeaderLine(string $header): string
+    {
+        return ServerRequestFactory::fromGlobals()->getHeaderLine($header);
     }
 
     private function getFormPostParams(): array
